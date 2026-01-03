cf3cacdc38146fd10a6652231667e40abcd08316
Isobel <173300585+isobel-p@users.noreply.github.com>
2026-01-03 12:34:06 +0000

    add jump display bar
    finally!

---
+++ jumps.gd
@@ -0,0 +1,14 @@
+extends HBoxContainer
+@onready var player := get_tree().get_first_node_in_group("player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func update_jumps():
+	for i in get_child_count():
+		get_child(i).visible = player.jumps > i	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/player.gd
+++ scripts/player.gd
@@ -6,9 +6,12 @@ const JUMP_VELOCITY = -400.0
 
 var max_jumps: int = 3
 var jumps: int = max_jumps
+@onready var bar = $Jumps
 
 func _ready() -> void:
 	EventController.connect("powerup", on_powerup)
+	global_position.x = 100
+	global_position.y = -100
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -16,11 +19,14 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	else:
 		jumps = max_jumps
+		bar.update_jumps()
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and jumps > 0:
 		velocity.y = JUMP_VELOCITY
 		jumps -= 1
+		bar.update_jumps()
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -29,6 +35,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if global_position.y > 120:
+		global_position.x = 100
+		global_position.y = -100
 
 	move_and_slide()
 
